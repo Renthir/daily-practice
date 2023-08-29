0660cae7f73682e03c2b843218f8d3afa6deb265
Seth Reid <reid.seth.96@gmail.com>
2023-08-29 11:54:04 -0600

    did some code practice problems to refresh my knowledge

--- daily-practice.py
+++ daily-practice.py
@@ -5,3 +5,18 @@
 c = 5
 
 print(1 + c)
+
+def even_or_odd(number):
+    if number % 2 ==  0:
+        return "Even"
+    else:
+        return "Odd"
+    
+
+def find_smallest_int(arr):
+    small = arr[0]
+    for num in arr:
+        if num < small:
+            small = num
+    return small
+
