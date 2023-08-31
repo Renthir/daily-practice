59bd81a21b9a22b00af57739aec0f8c7e40b2c88
Seth Reid <reid.seth.96@gmail.com>
2023-08-31 14:33:04 -0600

    more practice problems done

--- daily-practice.py
+++ daily-practice.py
@@ -20,3 +20,25 @@ def find_smallest_int(arr):
             small = num
     return small
 
+
+def is_even(n): 
+    return True if n % 2 == 0 else False 
+
+
+def remove_char(s):
+    new_s = s[1:-1]
+    return new_s
+
+
+def stringy(size):
+    num = 1
+    string = ""
+    while num <= size:
+        if num % 2 != 0:
+            string += "1"
+        else:
+            string += "0"
+        num += 1
+            
+    return string
+        
