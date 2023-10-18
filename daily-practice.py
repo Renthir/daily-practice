173bcc63b38bf943f911ca089ced16a7b36ecdc7
Seth Reid <reid.seth.96@gmail.com>
2023-10-17 21:29:19 -0600

    Another day of practice

--- daily-practice.py
+++ daily-practice.py
@@ -286,3 +286,54 @@ def exactly_one_topping(ketchup, mustard, onion):
     sum_of = int(ketchup) + int(mustard) + int(onion)
     return True if sum_of == 1 else False
 
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    if len(L) < 2:
+        return None
+    return L[1] 
+
+
+def losing_team_captain(teams):
+    """Given a list of teams, where each team is a list of names, return the 2nd player (captain)
+    from the last listed team
+    """
+    return teams[-1][1]
+
+def purple_shell(racers):
+    """Given a list of racers, set the first place racer (at the front of the list) to last
+    place and vice versa.
+    
+    >>> r = ["Mario", "Bowser", "Luigi"]
+    >>> purple_shell(r)
+    >>> r
+    ["Luigi", "Bowser", "Mario"]
+    """
+    first = racers.pop(0)
+    last = racers.pop(-1)
+    racers.append(first)
+    racers.insert(0, last)
+    
+a = [1, 2, 3]
+b = [1, [2, 3]]
+c = []
+d = [1, 2, 3][1:]
+
+# Put your predictions in the list below. Lengths should contain 4 numbers, the
+# first being the length of a, the second being the length of b and so on.
+lengths = [3, 2, 0, 2]
+
+def fashionably_late(arrivals, name):
+    """Given an ordered list of arrivals to the party and a name, return whether the guest with that
+    name was fashionably late.
+    """
+    index_n = arrivals.index(name)
+    if index_n >= (len(arrivals) / 2):
+        if index_n != len(arrivals) - 1:
+            return True
+        else:
+            return False
+    else:
+        return False
+    
