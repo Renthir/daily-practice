0718871a298d7b904f7d4f09c6e09c0f2d156a28
Seth Reid <reid.seth.96@gmail.com>
2023-10-22 23:30:36 -0600

    more practice

--- daily-practice.py
+++ daily-practice.py
@@ -337,5 +337,24 @@ def fashionably_late(arrivals, name):
     else:
         return False
     
-list1 = []
-list1.upper()
+# list1 = []
+# list1.upper()
+
+def has_lucky_number(nums):
+    """Return whether the given list of numbers is lucky. A lucky list contains
+    at least one number divisible by 7.
+    """
+    for num in nums:
+        if num % 7 == 0:
+            return True
+    return False
+
+def elementwise_greater_than(L, thresh):
+    """Return a list with the same length as L, where the value at index i is 
+    True if L[i] is greater than thresh, and False otherwise.
+    
+    >>> elementwise_greater_than([1, 2, 3, 4], 2)
+    [False, False, True, True]
+    """
+    return [num > thresh for num in L]
+
